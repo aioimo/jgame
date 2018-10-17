03961dd4eb6bfee11d907f6bd634455de1e39b28
Anthony Ioimo <anthonylioimo@gmail.com>
2018-10-17 17:57:58 +0200

    added divs and link functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
 console.log('Hello Berlin')
+
+function goToPage(link) {
+  var goTo = $('[data-page=' + link + ']');    //selects DIV with data-page attribute = "link"
+  console.log("GoTo",goTo);
+  goTo.siblings().hide();
+  goTo.show();
+
+  //Highlights which page we are on
+  $('li.nav-item').each(function(){
+    var href = $(this).find('a.nav-link').attr('href');
+    if (href == link) {
+      $(this).addClass('active');
+    } else {
+      $(this).removeClass('active');
+    }
+  })
+}
+
+$('a').click(function(e){
+  e.preventDefault()
+  var link = $(this).attr('href');    //find the href of the clicked 'a' element
+  goToPage(link);                   //goes to that link using above function
+})
+
+goToPage('home');
+
+//add the class "active" in the navbar upon click (and removing it)
